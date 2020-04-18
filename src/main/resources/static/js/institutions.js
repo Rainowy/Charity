8cf109da2aa5564e4ec9cc542febe933118fd6ab
Rainowy <tomasz.czarny.czarnecki@gmail.com>
2020-04-18 21:03:41 +0200

    Edit button disabling/enbabling input in institutions is working

--- src/main/resources/static/js/institutions.js
+++ src/main/resources/static/js/institutions.js
@@ -1,37 +1,7 @@
-$('#hideSecond').hide();
-// $('td').click(function () {
-//     var checked = $('input', this).is(':checked');
-//     $('span', this).text(checked ? ' Tak ' : ' Nie ');
-// });
-
-
-
-// let kupa = $('.name');
-// if(kupa.val("disabled",true)){
-//     console.log("NIE ZAZNACZONY")
-
-$("#edit").click(function () {
-//
-//     // if (document.querySelector(".nazwa").disabled = false) {        //wyłącza submit button gdy edycja wyłączona
-//         document.querySelector(".nazwa").disabled = true
-//     // }
-//
-//     console.log("ZMIANA")
-
-
-
-   $(this).closest('tr').find('input.nazwa').toggle("disabled");
-
-    // if(kot == true){
-    //     console.log("NIE ZAZNACZONY")
-    // }
-    // $(this).closest('td').next('td').next('td').find('input').prop("disabled", !this.checked);
+const editButtons = document.querySelectorAll('#edit')
+editButtons.forEach(function (currentBtn) {
+    currentBtn.addEventListener('click', function () {
+        $(this).closest('tr').find('input#name').prop("disabled", (_, val) => !val);
+        $(this).closest('tr').find('input#description').prop("disabled", (_, val) => !val);
+    })
 });
-// $("#hide").click(function () {
-//     $('#hideFirst').toggle();
-//     $('#hideSecond').toggle();
-// });
-// $(document).ready(function() {
-//     $('#dataTable2').DataTable();
-//     $('#dataTable').DataTable();
-// });
